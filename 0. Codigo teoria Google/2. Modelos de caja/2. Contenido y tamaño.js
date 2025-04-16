0210e5c0cbf11f01c857cd020791e1c988f9d7ee
Dayvis <amdayvis39@gmail.com>
2025-04-16 17:29:24 -0500

    16/04/2025

--- 0. Codigo teoria Google/2. Modelos de caja/2. Contenido y tamaño.js
+++ 0. Codigo teoria Google/2. Modelos de caja/2. Contenido y tamaño.js
@@ -1,20 +1,10 @@
-const parentBox = document.querySelector('[data-element="parent-box"');
-const heightLabel = document.querySelector('[data-element="height-label"]')
-const widthLabel = document.querySelector('[data-element="width-label"]') 
-const intrinsicSwitch = document.querySelector("#intrinsic-switch")
+const awesome = document.querySelector('[data-element="awesome"]');
+const intrinsicSwitch = document.querySelector("#intrinsic-switch");
 
-const observer = new ResizeObserver((observedItems) => {
-    const {borderBoxsize} = observedItems[0];
-
-    widthLabel.innerText = `${Math.round(borderBoxsize[0].inlineSize)}px`
-    heightLabel.innerText = `${Math.round(borderBoxsize[0].blockSize)}px`
-});
-
-observer.observe(parentBox);
-
+// Set sizing attribute based on switch
 intrinsicSwitch.addEventListener("change", () => {
-    parentBox.setAttribute(
-        "data-sizing",
-        intrinsicSwitch.checked ?  "intrinsic" : "extrinsic"
-    );
-}); 
+  awesome.setAttribute(
+    "data-sizing",
+    intrinsicSwitch.checked ? "intrinsic" : "extrinsic"
+  );
+});
