62efa661d1908127b68cc6606de82c280946ad45
Dayvis <amdayvis39@gmail.com>
2025-04-23 17:07:23 -0500

    23/04/2025

---
+++ 0. Codigo teoria Google/2. Modelos de caja/3. Contenido y tamaño 2.js
@@ -0,0 +1,20 @@
+const parentBox = document.querySelector('[data-element="parent-box"]');
+const heightLabel = document.querySelector('[data-element="height-label"]');
+const widthLabel = document.querySelector('[data-element="width-label"]');
+const intrinsicSwitch = document.querySelector("#intrinsic-switch");
+
+const observer = new ResizeObserver((observedItems) => {
+    const { borderBoxSize } = observedItems[0];
+
+    widthLabel.innerText = `${Math.round(borderBoxSize[0].inlineSize)}px`;
+    heightLabel.innerText = `${Math.round(borderBoxSize[0].blockSize)}px`;
+});
+
+observer.observe(parentBox);
+
+intrinsicSwitch.addEventListener("change", () => {
+    parentBox.setAttribute (
+        "data-sizing",
+        intrinsicSwitch.checked ? "intrinsic" : "extrinsic"
+    );
+});
